c570231200d485693900210b56905f755bbaaa01
miservilla <miservilla@gmail.com>
2017-01-28 12:17:05 -0700

    Change to all in one class.

--- src/Visualizer.java
+++ src/Visualizer.java
@@ -20,13 +20,100 @@ public class Visualizer {
         System.out.println("3 for 'In and out box.'");
         int in = input.nextInt();
         switch (in){
-            case 1: SquareBox.drawSquareBox(myDisplay, 0,
+            case 1: drawSquareBox(myDisplay, 0,
                     myDisplay.getHeight(), 0, myDisplay.getWidth());
                     break;
-            case 2: CircleCircle.drawCircleCircle(myDisplay);
+            case 2: drawCircleCircle(myDisplay);
                     break;
-            case 3: InAndOutBox.drawInBox(myDisplay, location);
+            case 3: drawInBox(myDisplay, location);
         }
 
     }
+    public static void drawSquareBox(Display myDisplay, int yTop,
+                                     int yBottom, int xLeft, int xRight) {
+        int y = yTop;
+        for (int x = xLeft; x < xRight; x += Visualizer.POINT_SIZE) {
+            myDisplay.drawNextPoint(x, y);
+        }
+        int x = xRight;
+        for (y = yTop; y < yBottom; y += Visualizer.POINT_SIZE) {
+            myDisplay.drawNextPoint(x, y);
+        }
+        y = yBottom;
+        for (x = xRight; x >= xLeft; x -= Visualizer.POINT_SIZE) {
+            myDisplay.drawNextPoint(x, y);
+        }
+        x = xLeft;
+        for (y = yBottom; y > yTop; y -= Visualizer.POINT_SIZE) {
+            myDisplay.drawNextPoint(x, y);
+        }
+    }
+    public static void drawCircleCircle(Display myDisplay) {
+        int centerXIC = myDisplay.getWidth() / 2;
+        int centerXOC;
+        int centerYIC = myDisplay.getHeight() / 2;
+        int centerYOC;
+
+        // Draw a circle starting at the top and going clock wise
+        double degAngIC = 270;
+        double degAngOC = 270;
+        double radiusIC = 120;
+        double radiusOC = 50;
+        double xIC, xOC, yIC, yOC, radAngIC, radAngOC;
+        while ( true ) {
+            radAngIC = (degAngIC * Math.PI) / 180;
+            xIC = centerXIC + radiusIC * Math.cos(radAngIC);
+            yIC = centerYIC + radiusIC * Math.sin(radAngIC);
+            myDisplay.drawNextPoint((int) xIC, (int) yIC);
+            centerXOC = (int) xIC;
+            centerYOC = (int) yIC;
+            degAngIC += 0.15;
+            radAngOC = (degAngOC * Math.PI) / 180;
+            xOC = centerXOC + radiusOC * Math.cos(radAngOC);
+            yOC = centerYOC + radiusOC * Math.sin(radAngOC);
+            myDisplay.drawNextPoint((int) xOC, (int) yOC);
+            degAngOC += 3;
+        }
+    }
+    public static void drawInBox(Display myDisplay, int[] location) {
+        int yTop = location[0];
+        int yBottom = location[1];
+        int xLeft = location[2];
+        int xRight = location[3];
+
+        while (xRight > 0){
+            drawSquareBox(myDisplay, yTop, yBottom, xLeft, xRight);
+            yTop += Visualizer.POINT_SIZE;
+            yBottom -= Visualizer.POINT_SIZE;
+            xLeft += Visualizer.POINT_SIZE;
+            xRight -= Visualizer.POINT_SIZE;
+        }
+        location[0] = yTop;
+        location[1] = yBottom;
+        location[2] = xLeft;
+        location[3] = xRight;
+        drawOutBox(myDisplay, location);
+    }
+    public static void drawOutBox(Display myDisplay, int[] location){
+        int yTop = location[0];
+        int yBottom = location[1];
+        int xLeft = location[2];
+        int xRight = location[3];
+
+        while (xLeft > 0){
+            drawSquareBox(myDisplay, yTop, yBottom, xLeft, xRight);
+            yTop -= Visualizer.POINT_SIZE;
+            yBottom += Visualizer.POINT_SIZE;
+            xLeft -= Visualizer.POINT_SIZE;
+            xRight += Visualizer.POINT_SIZE;
+        }
+        location[0] = yTop;
+        location[1] = yBottom;
+        location[2] = xLeft;
+        location[3] = xRight;
+
+        drawInBox(myDisplay, location);
+    }
+
+
 }
